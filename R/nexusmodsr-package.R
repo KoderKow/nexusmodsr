e60abcc3796368a4112c0cf940ec718dae36837b
KoderKow <koderkow@gmail.com>
2021-07-26 18:22:02 -0500

    Initial commit

---
+++ R/nexusmodsr-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+global_variables <- c(".", "id1", "id2", "make_get_request")
+
+globalVariables(global_variables)
---
+++ data-raw/base_url.R
@@ -0,0 +1,3 @@
+## code to prepare `base_url` dataset goes here
+base_url <- "https://api.nexusmods.com/v1/games"
+usethis::use_data(base_url, overwrite = TRUE, internal = TRUE)
